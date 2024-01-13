50635701c1e6363e1066c787231deedba8efb854
Mahesh Pawar <pawarmahesh723@gmail.com>
2024-01-13 14:00:56 +0530

    Implement Random RGB and HEX Color Generator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Accordion from './components/accordion';
+import RandomColor from './components/hex-color';
 
 function App() {
   return (
     <div className="App">
       {/* Accorion Component */}
       <Accordion />
+
+      {/* Random Color Component */}
+      <RandomColor/>
     </div>
   );
 }
